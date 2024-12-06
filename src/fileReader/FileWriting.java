07510fafa4ce5b4d2e6cb0ed874ed4da2afd48e9
Jojoseja <jfeyjoo@gmail.com>
2024-12-07 00:17:55 +0100

    Updatew

--- src/fileReader/FileWriting.java
+++ src/fileReader/FileWriting.java
@@ -2,16 +2,16 @@ package fileReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+
 public class FileWriting{
 	public static void main(String[] args) {
-		String dir = "C:\\Users\\Jojo\\Documents\\GitHub\\Teoria01\\src\\fileReader";
+		String dir = "./src/fileReader";
 		String fileName = "filename.txt";
 		try {
-			File file = new File (dir, "filename.txt" );
-			FileWriter myWriter = new FileWriter(file);
-			myWriter.write("This is printing in the file");
-			myWriter.close();
+			File file = new File (dir, fileName);
+			FileWriter owo = new FileWriter(file);
+			owo.write("This is printing in the file");
+			owo.close();
 			System.out.println("uwu");
 		} catch	(IOException e) {
 			System.out.println("Error :(");
